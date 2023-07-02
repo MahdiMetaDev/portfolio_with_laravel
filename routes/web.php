1e4f6584a01c9baf2ccf2687b47c1e3e4b42e7d4
mahdi <mmf1370py@gmail.com>
2023-07-01 22:27:36 -0700

    add laravel/breeze blade.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\PortfolioController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Web\BlogController;
 use App\Http\Controllers\Web\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -17,5 +18,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])->name('root');
-Route::get('portfolio', [PortfolioController::class, 'index'])->name('portfolio.index');
+Route::get('portfolio', [PortfolioController::class])->name('portfolio.index');
 Route::resource('blog', BlogController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
